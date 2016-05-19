884f1c3f8720841ae28287f547543faaab0688d3
Dmitriy <kelegorm@users.noreply.github.com>
2016-05-19 22:54:37 +0300

    Update content-gallery.php

--- content-gallery.php
+++ content-gallery.php
@@ -36,15 +36,21 @@ $moretag = $options[ 'more_tag_text' ];
                 $total_images = count( $images );
                 $image = array_shift( $images );
 
-                $image_img_tag = wp_get_attachment_image( $image->ID, 'full' );
-				?>
-                ALALALA!
-                <p><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'adventurous' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php echo $image_img_tag; ?></a></p>
-                <p><em><?php printf( _n( 'This gallery contains <a %1$s>%2$s photo</a>.', 'This gallery contains <a %1$s>%2$s photos</a>.', $total_images, 'adventurous' ), 'href="' . esc_url( get_permalink() ) . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'adventurous' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"', number_format_i18n( $total_images ) ); ?></em></p> 
+                $image_img_tag = wp_get_attachment_image( $image->ID, 'slider' );
+            ?>
+            
+                <p>
+                    <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'adventurous' ), the_title_attribute( 'echo=0' ) ) ); ?>">
+                        <?php echo $image_img_tag; ?>
+                    </a>
+                </p>
+                <p>
+                    <em><?php printf( _n( 'This gallery contains <a %1$s>%2$s photo</a>.', 'This gallery contains <a %1$s>%2$s photos</a>.', $total_images, 'adventurous' ), 'href="' . esc_url( get_permalink() ) . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'adventurous' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"', number_format_i18n( $total_images ) ); ?></em>
+                </p> 
                 
 			<?php else :
-            	
-				the_content( $moretag ); 
+         
+				the_content( $moretag );
 				wp_link_pages( array( 
 					'before'		=> '<div class="page-link"><span class="pages">' . __( 'Pages:', 'adventurous' ) . '</span>',
 					'after'			=> '</div>',
@@ -53,7 +59,6 @@ $moretag = $options[ 'more_tag_text' ];
 				) );
 				
           	endif; ?>
-eeeee!!!!!!!
              
         </div><!-- .entry-content -->    
             
